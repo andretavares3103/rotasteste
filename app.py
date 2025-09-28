fd0bc1d0bf96743418157d5fc00d933231e435cc
andretavares3103 <andre.mtavares1@gmail.com>
2025-09-28 19:08:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from email.mime.text import MIMEText
 PORTAL_EXCEL = "portal_atendimentos_clientes.xlsx"  # ou o nome correto do seu arquivo de clientes
 PORTAL_OS_LIST = "portal_atendimentos_os_list.json" # ou o nome correto da lista de OS (caso use JSON, por exemplo)
 
-st.set_page_config(page_title="BELO HORIZONTE || Otimização Rotas Vavivê", layout="wide")
+st.set_page_config(page_title="TESTE || Otimização Rotas Vavivê", layout="wide")
 
 ACEITES_FILE = "aceites.xlsx"
 ROTAS_FILE = "rotas_bh_dados_tratados_completos.xlsx"
@@ -1178,7 +1178,7 @@ if not st.session_state.admin_autenticado:
     st.markdown("""
         <div style='display:flex;align-items:center;gap:16px'>
             <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
-            <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || PORTAL DE ATENDIMENTOS</span>
+            <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>TESTE || PORTAL DE ATENDIMENTOS</span>
         </div>
         <p style='color:#666;font-size:1.08em;margin:8px 0 18px 0'>
             Consulte abaixo os atendimentos disponíveis!
@@ -1517,7 +1517,7 @@ with tabs[0]:
     st.markdown("""
         <div style='display:flex;align-items:center;gap:16px'>
             <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
-            <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || PORTAL DE ATENDIMENTOS</span>
+            <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>TESTE || PORTAL DE ATENDIMENTOS</span>
         </div>
         <p style='color:#666;font-size:1.08em;margin:8px 0 18px 0'>
             Consulte abaixo os atendimentos disponíveis!
@@ -1838,6 +1838,7 @@ with tabs[6]:
             total_linhas = len(df_view)
             divergentes = int(df_view["Divergência"].sum()) if "Divergência" in df_view else 0
             st.caption(f"Linhas exibidas: {total_linhas} | Divergências: {divergentes}")
+
 
 
 
